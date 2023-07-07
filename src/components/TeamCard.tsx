560e3928450804649352869ea91382fceb179d9e
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2023-07-07 18:51:31 +0800

    add animation

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -1,21 +1,36 @@
 import { Card, CardBody, Stat, Flex, StatLabel, StatHelpText, StatArrow, StatNumber, Box, CardProps } from "@chakra-ui/react";
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
 
 const TeamCard = forwardRef((props: { name: string, money: number, diff?: number } & CardProps, ref) => {
     const { name, money, diff } = props;
 
     const showDiff = typeof diff === 'number' && diff !== 0;
 
+    const [arrowUp, setArrowUp] = useState(false);
+    useEffect(() => {
+        if (showDiff) {
+            if (diff && diff > 0) {
+                setArrowUp(true);
+            } else if (diff && diff < 0) {
+                setArrowUp(false);
+            }
+        }
+    }, [diff, showDiff]);
+
     return <Card {...props} ref={ref}>
         <CardBody>
             <Stat>
                 <Flex justify='space-between'>
                     <Box>
-                        <StatLabel fontSize={showDiff ? 'sm' : '2xl'}>{name}</StatLabel>
-                        {showDiff ? <StatHelpText marginBottom={0}>
-                            {diff > 0 ? <StatArrow type='increase' /> : <StatArrow type='decrease' />}
-                            {diff.toLocaleString()}
-                        </StatHelpText> : null}
+                        <StatLabel transition={'font-size .5s ease-out'} fontSize={showDiff ? 'sm' : '2xl'}>{name}</StatLabel>
+                        <StatHelpText transition={'font-size .5s ease-out'} fontSize={showDiff ? 'sm' : '0px'}
+                            marginBottom={0}>
+                            {arrowUp ?
+                                <StatArrow boxSize={showDiff ? '1em' : '0px'} type='increase' /> :
+                                <StatArrow boxSize={showDiff ? '1em' : '0px'} type='decrease' />
+                            }
+                            {diff?.toLocaleString()}
+                        </StatHelpText>
                     </Box>
                     <Box>
                         <StatNumber>{money.toLocaleString()}</StatNumber>
